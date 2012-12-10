dd8c1996e23a58abed224ff0de598e6b0e6f5261
alexbirukov <lord_maloy@mail.ru>
2012-12-11 00:12:51 +0600

    Update Russian language
    Update Russian language

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,7 +7,7 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-06-18'
+		'release-date' => '2012-12-11'
 	);
 
 	/**
@@ -18,31 +18,34 @@
 		'CKEditor File Browser' => 
 		'Браузер файлов CKEditor',
 
-		'Permitted sections for the file browser:' => 
-		'Разделы доступные для браузера файлов:',
-
-		'Link templates:' => 
-		'Шаблоны ссылок:',
+		'Field to display as name' => 
+		'Поле для отображения в качестве имени',
 
 		'Link template' => 
 		'Шаблон ссылки',
 
-		'Use {$id} for the entry ID, and {$fieldname} for field-placeholders. If the field has a handle, this is automatically used.' => 
-		'Используйте переменную {$id} для вывода ID записи и {$fieldname} для подстановки значения поля.',
+		'Link templates:' => 
+		'Шаблоны ссылок:',
 
-		'Section to get the entries from' => 
-		'Раздел из которого производить выбор записей',
-
-		'Field to display as name' => 
-		'Поле для отображения в качестве имени',
+		'Permitted sections for the file browser:' => 
+		'Разделы доступные для браузера файлов:',
 
 		'Section' => 
 		'Раздел',
 
+		'Section to get the entries from' => 
+		'Раздел из которого производить выбор записей',
+
 		'There are no sections available. Please select which sections are permitted to use the CKEditor file upload feature in the Symphony System Preferences.' => 
 		'Не указаны доступные разделы. Пожалуйста, укажите в настройках системы разделы, которые доступны для загрузки файлов через CKEditor.',
 
+		'Use {$id} for the entry ID, and {$fieldname} for field-placeholders. If the field has a handle, this is automatically used.' => 
+		'Используйте переменную {$id} для вывода ID записи и {$fieldname} для подстановки значения поля.',
+
 		'create new' => 
 		'добавить',
+
+		'Styles: (one style per line: <code>h3.example { color: #f00; background: #0f0; }</code>) Class name is converted to name (h3.hello-world = Hello World).' => 
+		'Стили: (один стиль в строке: <code>h3.example { color: #f00; background: #0f0; }</code>) имя класса ковертируется в имя (h3.hello-world = Hello World).',
 
 	);
